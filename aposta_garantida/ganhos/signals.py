7f6a659c3bdd9165735acc3dbcb7b8153f4cc4a7
FernandoAraujoMarques <61606557+FernandoAraujoMarques@users.noreply.github.com>
2024-01-28 13:40:56 -0300

    Update signals.py

--- aposta_garantida/ganhos/signals.py
+++ aposta_garantida/ganhos/signals.py
@@ -1,22 +1,42 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from ganhos.models import Member, Saldo 
+from blank.models import Member, Saldo 
+import logging
+
+logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=Member)
 def novo_member_criado(sender, instance, created, **kwargs):
     if created:
-        # Subtrair o valor da aposta do saldo da casa
-        casa = instance.casa
-        valor_aposta = instance.valor
-        status = instance.status
+        processar_nova_aposta(instance)
 
-        # Certifique-se de lidar com situações em que o Saldo para a casa ainda não existe
-        saldo_casa, created = Saldo.objects.get_or_create(casa=casa)
+@receiver(post_save, sender=Member)
+def member_atualizado(sender, instance, **kwargs):
+    if instance.status == 'ganhou':
+        processar_aposta_ganha(instance)
 
-        # Atualizar o saldo subtraindo o valor da aposta
-        saldo_casa.valor -= valor_aposta
-        saldo_casa.save()
+def processar_nova_aposta(member_instance):
+    casa = member_instance.casa
+    valor_aposta = member_instance.valor
 
-        #print(f"Novo Member criado: {instance} com valor de aposta: {valor_aposta}. Saldo atualizado para a casa {casa}: {saldo_casa.valor}")
+    # Certifique-se de lidar com situações em que o Saldo para a casa ainda não existe
+    saldo_casa, created = Saldo.objects.get_or_create(casa=casa)
 
+    # Atualizar o saldo subtraindo o valor da aposta
+    saldo_casa.valor -= valor_aposta
+    saldo_casa.save()
 
+    logger.info(f"Nova aposta criada: {member_instance} com valor: {valor_aposta}. Saldo atualizado para a casa {casa}: {saldo_casa.valor}")
+
+def processar_aposta_ganha(member_instance):
+    casa_ganhadora = member_instance.casa
+    valor_ganho = member_instance.valor * member_instance.odd
+
+    # Certifique-se de lidar com situações em que o Saldo para a casa ganhadora ainda não existe
+    saldo_casa_ganhadora, created = Saldo.objects.get_or_create(casa=casa_ganhadora)
+
+    # Atualizar o saldo adicionando o valor ganho
+    saldo_casa_ganhadora.valor += valor_ganho
+    saldo_casa_ganhadora.save()
+
+    logger.info(f"Aposta ganha: {member_instance}. Saldo atualizado para a casa {casa_ganhadora}: {saldo_casa_ganhadora.valor}")
