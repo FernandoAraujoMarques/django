c67c36efc3672bfc89e2ced9a0ae19fb6aa726a8
Fernando Marques <fernanndomarques@terra.com.br>
2024-02-19 18:07:27 -0300

    news

--- aposta_garantida/ganhos/admin.py
+++ aposta_garantida/ganhos/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Member, Saldo, Aposta, Contas
+from .models import Member, Saldo, Aposta, Contas, Recebido
 
 # Register your models here.
 admin.site.register(Member)
 admin.site.register(Saldo)
 admin.site.register(Aposta)
 admin.site.register(Contas)
+admin.site.register(Recebido)
